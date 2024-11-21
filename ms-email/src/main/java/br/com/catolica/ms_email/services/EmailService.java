c61d4251e7b1d3bf93c627878132c95b4992ddda
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-11-21 11:29:57 -0300

    feat: cria service de envio de email

---
+++ ms-email/src/main/java/br/com/catolica/ms_email/services/EmailService.java
@@ -0,0 +1,27 @@
+package br.com.catolica.ms_email.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue.name}")
+    public void sendMail(String email) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom("gustavoaraujohab@gmail.com");
+            mailMessage.setTo(email);
+            mailMessage.setSubject("Empréstimo realizado com sucesso!");
+            mailMessage.setText("Parabéns, você acaba de realizar um empréstimo!");
+            javaMailSender.send(mailMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
