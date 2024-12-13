82a43422c7280450cc42a15bd174167a6d8daa48
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-12-13 12:34:48 -0300

    feat: sobe o spring security

--- ms-gateway/src/main/java/br/com/catolica/ms_gateway/config/SecurityConfig.java
+++ ms-gateway/src/main/java/br/com/catolica/ms_gateway/config/SecurityConfig.java
@@ -5,20 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-  @Autowired
-  private JwtConfig jwtConfig;
   
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-      .authorizeHttpRequests(req -> req
-        .anyRequest().permitAll()
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(req -> req
+            .anyRequest().permitAll()
       )
       .build();
   }
